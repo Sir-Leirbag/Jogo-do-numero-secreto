b1737bc8b685e4c7254ce79e5a42f6232e213867
Sir-Leirbag <gabrielsbisppo@gmail.com>
2026-04-04 11:48:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function verificarChute () {
     if (chute == numeroSecreto) {
         exibirTextoNaTela ('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela ('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     }else{

e2149aade9f7f83e3c19838948074ce455916d53
Sir-Leirbag <gabrielsbisppo@gmail.com>
2026-04-05 17:00:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio ();
 let tentativas = 1;
-
 function exibirTextoNaTela (tag, texto) {
     let campo = document.querySelector (tag);
     campo.innerHTML = texto;
@@ -14,21 +14,18 @@ function exibirTextoNaTela (tag, texto) {
         console.log("Web Speech API não suportada neste navegador.");
     }
 }
-
 function exibirMensagemInicial (){
-    exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');   
+    exibirTextoNaTela('h1', 'Jogo do numero secreto');
+    exibirTextoNaTela('p', 'Escolha um numero entre 1 e 10');   
 }
-
 exibirMensagemInicial ();
-
 function verificarChute () {
     let chute = document.querySelector('input').value;
 
     if (chute == numeroSecreto) {
         exibirTextoNaTela ('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativas = `Você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativas = `você descobriu o numero secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela ('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
@@ -43,20 +40,23 @@ function verificarChute () {
     console.log(chute == numeroSecreto);
 }
 function gerarNumeroAleatorio() {
-    let numeroEscolhido = parseInt(Math.random () * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random () * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
         return gerarNumeroAleatorio
     }else{
-        listaDeNumerosSorteados.push(numeroEscolhido)
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
         return numeroEscolhido;
     }
 }
-
 function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
-
 function reiniciarJogo () {
     numeroSecreto = gerarNumeroAleatorio ();
     limparCampo ();
